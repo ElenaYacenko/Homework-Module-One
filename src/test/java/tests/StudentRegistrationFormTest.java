649217418808d1bdcbfed67397dc2193823cb6b2
ElenaYacenko <149763977+ElenaYacenko@users.noreply.github.com>
2026-05-30 10:06:33 +0300

    feat: adding negative tests, changing the redme file

--- src/test/java/tests/StudentRegistrationFormTest.java
+++ src/test/java/tests/StudentRegistrationFormTest.java
@@ -29,12 +29,11 @@ public class StudentRegistrationFormTest extends tests.TestBase {
         $("[id=react-select-3-input]").setValue("Haryana").pressEnter();
         $("[id=react-select-4-input]").setValue("Karnal").pressEnter();
 
-
         $("[id=submit]").scrollTo().click();
 
         $(".modal-content").shouldBe(visible);
-        $("[id=example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
 
+        $("[id=example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
         $x("//tr[td[contains(., 'Student Name')]]/td[2]").shouldHave(text("Elena Ya"));
         $x("//tr[td[contains(., 'Student Email')]]/td[2]").shouldHave(text("xxx@ya.ru"));
         $x("//tr[td[contains(., 'Gender')]]/td[2]").shouldHave(text("Female"));
@@ -44,7 +43,6 @@ public class StudentRegistrationFormTest extends tests.TestBase {
         $x("//tr[td[contains(., 'Hobbies')]]/td[2]").shouldHave(text("Reading"));
         $x("//tr[td[contains(., 'Address')]]/td[2]").shouldHave(text("RU"));
         $x("//tr[td[contains(., 'State and City')]]/td[2]").shouldHave(text("Haryana Karnal"));
-
     }
 
     @Test
@@ -53,21 +51,50 @@ public class StudentRegistrationFormTest extends tests.TestBase {
 
         $("[id=firstName]").setValue("Elena");
         $("[id=lastName]").setValue("Ya");
-        $("[value=Male]").click();
+        $("[id='genterWrapper'] input[value='Male']").click();
         $("[id=userNumber]").setValue("9999999999");
 
         $("[id=submit]").scrollTo().click();
 
         $(".modal-content").shouldBe(visible);
-        $("[id=example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
 
+        $("[id=example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
         $x("//tr[td[contains(., 'Student Name')]]/td[2]").shouldHave(text("Elena Ya"));
         $x("//tr[td[contains(., 'Gender')]]/td[2]").shouldHave(text("Male"));
         $x("//tr[td[contains(., 'Mobile')]]/td[2]").shouldHave(text("9999999999"));
+    }
 
-        //$("[id=output] [id=name]").shouldHave(text("Elena Ya"));
-        //$("[id=output] [id=email]").shouldHave(text("Male"));
-        //$("[id=output] [id=currentAddress]").shouldHave(text("9999999999"));
+    @Test
+    void negativeTest_FirstNameOneCharacterRejected() {
+        open("/automation-practice-form");
+
+        $("[id=firstName]").setValue("E");
+        $("[id=lastName]").setValue("Ya");
+        $("[id='genterWrapper'] input[value='Other']").click();
+        $("[id=userNumber]").setValue("9999999999");
+
+        $("[id=submit]").click();
+
+        $("[id=firstName]").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $(".modal-content").shouldNotBe(visible);
+    }
+
+    @Test
+    void negativeTest_LastNameExceedsMaxLengthShowsRedBorder() {
+        open("/automation-practice-form");
+
+        $("[id=firstName]").setValue("Elena");
+        $("[id=lastName]").setValue("YaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYa" +
+                "YaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYa" +
+                "YaYaYaYaYaYa-YaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYa" +
+                "123456"); //257 символов
+        $("[id='genterWrapper'] input[value='Male']").click();
+        $("[id=userNumber]").setValue("9999999999");
+
+        $("[id=submit]").click();
+
+        $("[id=lastName]").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $(".modal-content").shouldNotBe(visible);
     }
 
     @Test
@@ -83,7 +110,6 @@ public class StudentRegistrationFormTest extends tests.TestBase {
         $("[name=gender][value=Male]").shouldNotBe(checked);
         $("[name=gender][value=Female]").shouldNotBe(checked);
         $("[name=gender][value=Other]").shouldNotBe(checked);
-
         $(".modal-content").shouldNotBe(visible);
     }
 
@@ -93,13 +119,12 @@ public class StudentRegistrationFormTest extends tests.TestBase {
 
         $("[id=firstName]").setValue("Elena");
         $("[id=lastName]").setValue("Ya");
-        $("[value=Male]").click();
+        $("[id='genterWrapper'] input[value='Male']").click();
         $("[id=userNumber]").setValue("999999999"); // телефон из 9 символов
 
         $("[id=submit]").click();
 
         $("[id=userNumber]").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
-
         $(".modal-content").shouldNotBe(visible);
     }
 
@@ -109,13 +134,70 @@ public class StudentRegistrationFormTest extends tests.TestBase {
 
         $("[id=firstName]").setValue("Elena");
         $("[id=lastName]").setValue("Ya");
-        $("[value=Male]").click();
-        $("[id=userNumber]").setValue("ТелеНомера"); // телефон из 9 символов
+        $("[id='genterWrapper'] input[value='Female']").click();
+        $("[id=userNumber]").setValue("ТелеНомера"); // ввод в поле букв
 
         $("[id=submit]").click();
 
         $("[id=userNumber]").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $(".modal-content").shouldNotBe(visible);
+    }
 
+    @Test
+    void negativeTest_MissingFirstNameShowsRedBorder(){
+        open("/automation-practice-form");
+
+        $("[id=lastName]").setValue("Ya");
+        $("[id='genterWrapper'] input[value='Female']").click();
+        $("[id=userNumber]").setValue("9999999999");
+
+        $("[id=submit]").click();
+
+        $("[id=firstName]").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $(".modal-content").shouldNotBe(visible);
+    }
+
+    @Test
+    void negativeTest_MissingLastNameShowsRedBorder() {
+        open("/automation-practice-form");
+
+        $("[id=firstName]").setValue("Elena");
+        $("[id='genterWrapper'] input[value='Male']").click();
+        $("[id=userNumber]").setValue("9999999999");
+
+        $("[id=submit]").click();
+
+        $("[id=lastName]").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $(".modal-content").shouldNotBe(visible);
+    }
+
+    @Test
+    void negativeTest_MissingMobileShowsRedBorder() {
+        open("/automation-practice-form");
+
+        $("[id=firstName]").setValue("Elena");
+        $("[id=lastName]").setValue("Ya");
+        $("[id='genterWrapper'] input[value='Male']").click();
+
+        $("[id=submit]").click();
+
+        $("[id=userNumber]").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $(".modal-content").shouldNotBe(visible);
+    }
+
+    @Test
+    void negativeTest_UnselectedGenderShowsValidationError() {
+        open("/automation-practice-form");
+
+        $("[id=firstName]").setValue("Elena");
+        $("[id=lastName]").setValue("Ya");
+        $("[id=userNumber]").setValue("9999999999");
+
+        $("[id=submit]").click();
+
+        $("[name=gender][value=Male]").shouldNotBe(checked);
+        $("[name=gender][value=Female]").shouldNotBe(checked);
+        $("[name=gender][value=Other]").shouldNotBe(checked);
         $(".modal-content").shouldNotBe(visible);
     }
 }
