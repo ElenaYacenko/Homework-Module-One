d1266c273c482741ee0a1f5a52997bcc9ae5d8d2
ElenaYacenko <149763977+ElenaYacenko@users.noreply.github.com>
2026-06-04 16:00:58 +0300

    added tests to TextBoxTests

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,6 +1,8 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import tests.TestBase;
 
+import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -8,17 +10,61 @@ import static com.codeborne.selenide.Selenide.open;
 public class TextBoxTests extends TestBase {
 
     @Test
+    @DisplayName("Позитивный тест: форма с заполнеными полями")
     void successfulFillFormTest() {
         open("/text-box.html");
-        $("[id=userName]").setValue("Alex Black");
-        $("[id=userEmail]").setValue("alex@black.com");
-        $("[id=currentAddress]").setValue("first address 1");
-        $("[id=permanentAddress]").setValue("second address 2");
-        $("[id=submit]").click();
+        $("#userName").setValue("Alex Black");
+        $("#userEmail").setValue("alex@black.com");
+        $("#currentAddress").setValue("first address 1");
+        $("#permanentAddress").setValue("second address 2");
+        $("#submit").click();
 
         $("[id=output] [id=name]").shouldHave(text("Alex Black"));
         $("[id=output] [id=email]").shouldHave(text("alex@black.com"));
         $("[id=output] [id=currentAddress]").shouldHave(text("first address 1"));
         $("[id=output] [id=permanentAddress]").shouldHave(text("second address 2"));
+    }
+
+    @Test
+    @DisplayName("Негативный тест: отправка пустой формы")
+    void emptyFormTest() {
+        open("/text-box.html");
+
+        $("#submit").click();
+
+        $("[id=output] [id=name]").shouldHave(text("Name:"));
+        $("[id=output] [id=email]").shouldHave(text("Email:"));
+        $("[id=output] [id=currentAddress]").shouldHave(text("Current Address :"));
+        $("[id=output] [id=permanentAddress]").shouldHave(text("Permananet Address :"));
+    }
+
+    @Test
+    @DisplayName("Отправка формы с не заполнеными полями адресов")
+    void empryAddress() {
+        open("/text-box.html");
+        $("#userName").setValue("Alex Black");
+        $("#userEmail").setValue("alex@black.com");
+        $("#submit").click();
+
+        $("[id=output] [id=name]").shouldHave(text("Alex Black"));
+        $("[id=output] [id=email]").shouldHave(text("alex@black.com"));
+        $("[id=output] [id=currentAddress]").shouldHave(text("Current Address :"));
+        $("[id=output] [id=permanentAddress]").shouldHave(text("Permananet Address :"));
+    }
+
+    @Test
+    @DisplayName("Валидация: ошибка при вводе email без символа @")
+    void invalidEmailWithoutAtSymbolTest() {
+        open("/text-box.html");
+
+        // Заполняем форму
+        $("#userName").setValue("Елена");
+        $("#userEmail").setValue("test1mail.ru");
+        $("#currentAddress").setValue("Ля ля ФА");
+        $("#permanentAddress").setValue("До РЕ Ми Фа");
+
+        $("#submit").click();
+
+        $("#output").shouldNotHave(cssClass("has-content"));
     }
 }
