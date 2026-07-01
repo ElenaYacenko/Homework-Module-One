3fb8b11a2ebe7ef37bf54a9ca0fae97f025f2d44
ElenaYacenko <149763977+ElenaYacenko@users.noreply.github.com>
2026-07-01 12:31:38 +0300

    Added TextBoxPage

---
+++ src/test/java/pages/TextBoxPage.java
@@ -0,0 +1,66 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static testsData.FormTestData.resultTableClass;
+
+public class TextBoxPage {
+    //Элемены формы
+    private SelenideElement userNameInput = $("#userName");
+    private SelenideElement userEmailInput = $("#userEmail");
+    private SelenideElement userCurrentAddressInput = $("#currentAddress");
+    private SelenideElement userPermanentAddressInput = $("#permanentAddress");
+    private SelenideElement outputResults = $("#output");
+    private SelenideElement submitButton = $("#submit");
+
+
+    //Действия
+    public TextBoxPage openPage() {
+        open("/text-box.html");
+        return this;
+    }
+
+    public TextBoxPage typeUserNameInput(String value) {
+        userNameInput.setValue(value);
+        return this;
+    }
+
+    public TextBoxPage typeUserEmailInput(String value) {
+        userEmailInput.setValue(value);
+        return this;
+    }
+
+    public TextBoxPage typeCurrentAddressInput(String value) {
+        userCurrentAddressInput.setValue(value);
+        return this;
+    }
+
+    public TextBoxPage typePermanentAddressInput(String value) {
+        userPermanentAddressInput.setValue(value);
+        return this;
+    }
+
+    public TextBoxPage clickOnSubmitButton() {
+        submitButton.click();
+        return this;
+    }
+
+    //Проверки
+    /*Проверяет, что в блоке результатов отображается ожидаемое значение для указанного поля
+    (key   ID поля [id=name], value ожидаемое значение - значение переменной из файла FormTestData*/
+
+    public TextBoxPage checkField(String key, String value) {
+        outputResults.$(byId(key)).shouldHave(text(value));
+        return this;
+    }
+
+    public TextBoxPage verifyOutputNotDisplayed() {
+        outputResults.shouldNotHave(cssClass(resultTableClass));
+        return this;
+    }
+}
---
+++ src/test/java/testsData/FormTestData.java
@@ -0,0 +1,45 @@
+package testsData;
+
+public class FormTestData {
+    public static String
+
+            firstName = "Elena",
+            tooShortFirstName = "E",
+            lastName = "Ya",
+            tooLongLastName = "YaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYa" +
+                    "YaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYa" +
+                    "YaYaYaYaYaYa-YaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYaYa" +
+                    "1234567", // 257 символов
+            fullName = firstName + " " + lastName,
+
+            userEmail = "xxx@ya.ru",
+            emailWithoutAt = "test1mail.ru",
+            gender = "Female",
+            phoneNumber = "1234567890",
+            tooShortPhoneNumber = "123456789",       // 9 символов
+            phoneWithLetters = "ТелеНомера",// буквы в телефоне
+
+
+            currentAddress = "first address 1",
+            permanentAddress = "second address 2",
+
+
+            formTitle = "Student Registration Form",
+            successModalTitle = "Thanks for submitting the form",
+            resultTableClass = "has-content",
+
+
+            dobMonth = "March",
+            dobYear = "2020",
+            dobDay = "30",
+
+
+            state = "Haryana",
+            city = "Karnal",
+
+
+            subject = "Maths",
+            hobbies = "Reading",
+
+            testImagePath = "img/1.png";
+}
